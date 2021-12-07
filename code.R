b829ac8ff2652f40e2c0333a91ea74077eb2f652
PabloFdezm <pablo.fernandezmo@mail.udp.cl>
2021-12-07 11:09:56 -0300

    update

--- code.R
+++ code.R
@@ -11,11 +11,13 @@ patents_2000 = patents_2000 %>%
          n = c(rep(1:branches, times = 1, each = max_derwent), rep(branches+1,branches_res))
   )
 
+
+
 for (i in 1:(branches+1)) {
   if (i %% 2 == 0) {
     write.table(
       patents_2000 %>% filter(n == i) %>% select(complete_id),
-      paste0('branches/branch_A',100+i,'.txt'),
+      paste0('branches/A/branch_A',100+i,'.txt'),
       sep = '\t',
       row.names = FALSE,
       col.names = FALSE
@@ -23,7 +25,7 @@ for (i in 1:(branches+1)) {
   }else{
     write.table(
       patents_2000 %>% filter(n == i) %>% select(complete_id),
-      paste0('branches/branch_B',100+i,'.txt'),
+      paste0('branches/B/branch_B',100+i,'.txt'),
       sep = '\t',
       row.names = FALSE,
       col.names = FALSE
