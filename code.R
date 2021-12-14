8251290798d50133d55d4b66b596d7a6356d7631
PabloFdezm <pablo.fernandezmo@mail.udp.cl>
2021-12-14 18:24:20 -0300

    update

--- code.R
+++ code.R
@@ -11,8 +11,6 @@ patents_2000 = patents_2000 %>%
          n = c(rep(1:branches, times = 1, each = max_derwent), rep(branches+1,branches_res))
   )
 
-
-
 for (i in 1:(branches+1)) {
   if (i %% 2 == 0) {
     write.table(
@@ -30,5 +28,41 @@ for (i in 1:(branches+1)) {
       row.names = FALSE,
       col.names = FALSE
       )
+  }
+}
+
+
+##### Patents 1990
+
+patents_1990 <- read.csv("~/patents_1990.csv")
+
+max_derwent = 30000
+branches = nrow(patents_1990) %/% max_derwent
+branches_res = nrow(patents_1990) %% max_derwent
+
+patents_1990 = patents_1990 %>%
+  mutate(complete_id = paste0(country,patent_id,kind),
+         n = c(rep(1:branches, times = 1, each = max_derwent), rep(branches+1,branches_res))
+  )
+
+
+
+for (i in 1:(branches+1)) {
+  if (i %% 2 == 0) {
+    write.table(
+      patents_1990 %>% filter(n == i) %>% select(complete_id),
+      paste0('branches/A/branch_A',100+i,'.txt'),
+      sep = '\t',
+      row.names = FALSE,
+      col.names = FALSE
+    )
+  }else{
+    write.table(
+      patents_1990 %>% filter(n == i) %>% select(complete_id),
+      paste0('branches/B/branch_B',100+i,'.txt'),
+      sep = '\t',
+      row.names = FALSE,
+      col.names = FALSE
+    )
   }
 }
